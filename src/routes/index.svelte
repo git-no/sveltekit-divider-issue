6d9c4ff9d7f24677589ab4df4bb3ec1a26fb9d5b
Dr. No <mo+ita-git@depeche.email>
2022-03-15 20:22:40 +0100

    Initial commit sveltekit, tailwind

---
+++ src/routes/index.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+	let hasError = true;
+</script>
+
+<div class="min-h-screen bg-slate-600/10" class:bg-red-600/10={hasError}>
+	<h1 class="text-center">SvelteKit Tailwind Divider Class Issue</h1>
+</div>
---
+++ tailwind.config.cjs
@@ -0,0 +1,11 @@
+const defaultTheme = require('tailwindcss/defaultTheme')
+
+module.exports = {
+  content: ['./src/**/*.{html,js,svelte,ts}'],
+  theme: {
+    extend: {
+
+    },
+  },
+  plugins: [],
+}
